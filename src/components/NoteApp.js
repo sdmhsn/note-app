9cf3001e411e5a75be3a2e09e60ec2c6472e80a1
Saddam Husein <saddambinhusein2@gmail.com>
2022-08-13 18:33:43 +0800

    Added NoteInput component with addNote handler into NoteApp module

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import { getInitialData } from '../utils/initialData';
+import NoteInput from './NoteInput';
 import NoteList from './NoteList';
 
 class NoteApp extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            notes: getInitialData(),
-        };
-    }
-
-    render() {
-        return (
-            <div className="note-app__body">
-                <NoteList notes={this.state.notes} />
-            </div>
-        );
+    this.state = {
+      notes: getInitialData(),
     };
+
+    this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
+  }
+
+  onAddNoteHandler({ title, body }) {
+    this.setState((prevState) => {
+      return {
+        notes: [
+          ...prevState.notes,
+          {
+            id: +new Date(),
+            title,
+            body,
+            createdAt: '2022-04-14T04:27:34.572Z',
+            archived: false,
+          },
+        ],
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="note-app__body">
+        <NoteInput addNote={this.onAddNoteHandler} />
+        <h2>Active Note</h2>
+        <NoteList notes={this.state.notes} />
+      </div>
+    );
+  }
 }
 
 export default NoteApp;
