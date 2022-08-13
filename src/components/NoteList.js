8aa050bae5a02ac629092a80d0a277c63797e8a8
Saddam Husein <saddambinhusein2@gmail.com>
2022-08-14 01:41:31 +0800

    Modified NoteList module: Added id and onDelete property for handler and ternary operator conditional randering for empty note

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import NoteItem from './NoteItem';
 
-function NoteList({ notes }) {
-  return (
+function NoteList({ notes, onDelete }) {
+  return notes.length === 0 ? (
+    <div>
+      <p className="notes-list__empty-message">Empty Note</p>
+    </div>
+  ) : (
     <div className="notes-list">
       {notes.map((note) => (
-        <NoteItem key={note.id} {...note} />
+        <NoteItem key={note.id} id={note.id} onDelete={onDelete} {...note} />
       ))}
     </div>
   );
