25ada0022ce0db30cf33b60da49e6eacd645a3d6
Saddam Husein <saddambinhusein2@gmail.com>
2022-08-17 14:56:42 +0800

    Modified NoteList module: Created logics for Actived and Archived Note list

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import NoteItem from './NoteItem';
 
-function NoteList({ notes, onDelete }) {
-  return notes.length === 0 ? (
-    <div>
-      <p className="notes-list__empty-message">Empty Note</p>
-    </div>
-  ) : (
-    <div className="notes-list">
-      {notes.map((note) => (
-        <NoteItem key={note.id} id={note.id} onDelete={onDelete} {...note} />
-      ))}
-    </div>
+function NoteList({ notes, onDelete, onArchiveMove }) {
+  const archived = notes.filter((note) => note.archived);
+  const actived = notes.filter((note) => note.archived === false);
+
+  return (
+    <React.Fragment>
+      <h2>Actived Note</h2>
+      {actived.length !== 0 ? (
+        <div className="notes-list">
+          {actived.map((note) => (
+            <NoteItem
+              key={note.id}
+              id={note.id}
+              onDelete={onDelete}
+              onArchiveMove={onArchiveMove}
+              {...note}
+            />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p className="notes-list__empty-message">Actived Note Empty</p>
+        </div>
+      )}
+
+      <h2>Archived Note</h2>
+      {archived.length !== 0 ? (
+        <div className="notes-list">
+          {archived.map((note) => (
+            <NoteItem
+              key={note.id}
+              id={note.id}
+              onDelete={onDelete}
+              onArchiveMove={onArchiveMove}
+              {...note}
+            />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p className="notes-list__empty-message">Archived Note Empty</p>
+        </div>
+      )}
+    </React.Fragment>
   );
 }
 
