3b782623e030a16efe4dce607ce7930d4e5466c1
Saddam Husein <saddambinhusein2@gmail.com>
2022-08-17 16:54:13 +0800

    Modified NoteApp module: Created and added onArchiveMove event handler

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -15,32 +15,35 @@ class NoteApp extends React.Component {
     this.onSearchNoteHandler = this.onSearchNoteHandler.bind(this);
     this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
     this.onDeleteEventHandler = this.onDeleteEventHandler.bind(this);
+    this.onArchiveMoveEventHandler = this.onArchiveMoveEventHandler.bind(this);
   }
 
+  // Search Note
   onSearchNoteHandler(event) {
     const text = event.target.value.toLowerCase();
-    const data = getInitialData();
-    const tempStorage = []
+    const noteData = getInitialData();
+    const tempStorage = [];
 
-    data.forEach((item) => {
+    noteData.forEach((item) => {
       if (item.title.toLowerCase().includes(text)) {
-        tempStorage.push(item)
+        tempStorage.push(item);
 
         this.setState(() => {
-            return {
-              notes: tempStorage
-            };
+          return {
+            notes: tempStorage,
+          };
         });
       } else {
         this.setState(() => {
           return {
-            notes: tempStorage
+            notes: tempStorage,
           };
-      });
+        });
       }
     });
   }
 
+  // Add Note
   onAddNoteHandler({ title, body }) {
     this.setState((prevState) => {
       return {
@@ -58,10 +61,9 @@ class NoteApp extends React.Component {
     });
   }
 
+  // Delete Note
   onDeleteEventHandler(id) {
     const notes = this.state.notes.filter((note) => note.id !== id);
-
-    console.log(notes)
 
     this.setState(() => {
       return {
@@ -70,16 +72,41 @@ class NoteApp extends React.Component {
     });
   }
 
+  // Archive and Move Note
+  onArchiveMoveEventHandler(title, createdAt, body, id, archived) {
+    if (archived) {
+      const moves = this.state.notes.filter((note) =>
+        note.id === id ? (note.archived = false) : note.archived
+      );
+
+      this.setState(() => {
+        return {
+          ...moves,
+        };
+      });
+    } else {
+      const archieve = this.state.notes.filter((note) =>
+        note.id === id ? (note.archived = true) : note.archived
+      );
+
+      this.setState(() => {
+        return {
+          ...archieve,
+        };
+      });
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         <NoteSearch onKeyUp={this.onSearchNoteHandler} />
         <div className="note-app__body">
           <NoteInput addNote={this.onAddNoteHandler} />
-          <h2>Active Note</h2>
           <NoteList
             notes={this.state.notes}
             onDelete={this.onDeleteEventHandler}
+            onArchiveMove={this.onArchiveMoveEventHandler}
           />
         </div>
       </React.Fragment>
