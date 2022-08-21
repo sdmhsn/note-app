07cdd8346db1f92da6cc9e03142452dc974d8dc1
Saddam Husein <saddambinhusein2@gmail.com>
2022-08-21 14:26:52 +0800

    - Fixed bug on search features - Changed event and property

--- src/components/NoteSearch.js
+++ src/components/NoteSearch.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function NoteSearch({ onKeyUp }) {
+function NoteSearch({ search, onSearchChange }) {
   return (
     <div className="note-app__header">
       <h1>Notes</h1>
       <div className="note-search">
-        <input type="text" placeholder="Find note..." onKeyUp={onKeyUp} />
+        <input type="text" placeholder="Find note..." value={search} onChange={onSearchChange} />
       </div>
     </div>
   );
